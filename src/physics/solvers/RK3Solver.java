298cfccd1eea0af3ded0b8dc7aaacfadc94e956f
tamar <115258119+tamartrbl@users.noreply.github.com>
2023-05-08 15:01:42 +0200

    rk3 class added

---
+++ src/physics/solvers/RK3Solver.java
@@ -0,0 +1,14 @@
+package physics.solvers;
+
+import physics.vectors.StateVector;
+
+public class RK3Solver implements Solver{
+
+    public RK3Solver(){
+
+    }
+    
+    public StateVector solve() {
+        return null;
+    }
+}

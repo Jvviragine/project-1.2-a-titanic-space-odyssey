a803a32f797f03018fedfeebae8ee60b7248bf1a
ulasawczuk <sawczuk.ula@gmail.com>
2023-05-08 18:42:51 +0200

    Update RK3Solver.java

--- src/physics/solvers/RK3Solver.java
+++ src/physics/solvers/RK3Solver.java
@@ -2,11 +2,17 @@ package physics.solvers;
 
 import physics.functions.Function;
 import physics.vectors.StateVector;
+import physics.vectors.Vector;
 
 public class RK3Solver implements Solver{
 
-    public RK3Solver(){
-
+    StateVector y0;
+    double h;
+    double t0;
+    public RK3Solver(StateVector y0, double h, double t0){
+        this.y0 = y0;
+        this.h = h;
+        this.t0 = t0;
     }
 
     public StateVector solve(Function function, StateVector initialCondition, double t0, double tf, double stepSize) {

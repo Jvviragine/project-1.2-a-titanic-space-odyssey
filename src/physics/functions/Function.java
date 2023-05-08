a890d5e2c3cc9ca2677e49fdd059f3a67f3792f4
João Victor <j.coutinhoviragine@student.maastrichtuniversity.nl>
2023-05-08 15:26:26 +0200

    Changes the Definition of Function

--- src/physics/functions/Function.java
+++ src/physics/functions/Function.java
@@ -1,8 +1,9 @@
 package physics.functions;
 
+import physics.vectors.StateVector;
 import physics.vectors.Vector;
 
 public interface Function {
-    Vector apply();
+    public StateVector applyFunction(StateVector stateVector);
     // Maybe change it to Abstract  to it can be Inherited
 }

ed01107e0ba30eacd7363fd22b40426dd4bcd609
tamar <115258119+tamartrbl@users.noreply.github.com>
2023-05-08 23:14:54 +0200

    Comments, removed Euler constructor parameters

--- src/physics/solvers/EulerSolver.java
+++ src/physics/solvers/EulerSolver.java
@@ -5,32 +5,21 @@ import physics.vectors.Vector;
 import physics.vectors.StateVector;
 
 public class EulerSolver implements Solver{
-    StateVector y0;
-    StateVector fty;
-    double h;
-
     static double t;
+    
+    public EulerSolver(){
 
-    /**
-     * Constructor for an Euler solver
-     * @param y0 the initial state vector on which to perform Euler's method
-     * @param fty the derivative f(t,y) of the current state vector
-     * @param h the step size
-     * @param t0 the initial time
-     */
-
-    //y0, function f, h, t0
-    public EulerSolver(StateVector y0, StateVector fty, double h, double t0){
-        this.y0 = y0;
-        this.fty = fty;
-        this.h = h;
-        this.t = t0;
     }
 
     /**
      * Applies Euler's method to the current state vector
      *      yn+1 = yn + hf(t,y)
-     * @return the next state vector at state yn+1
+     * @param function the derivative function of the equation
+     * @param initialCondition the position and velocity at t0
+     * @param t0 the time from which the Euler steps are calculated
+     * @param tf the time until which the Euler steps are calculated
+     * @param stepSize the time interval at which evaluations of the derivative are made
+     * @return the state vector at the end of the time period tf - t0
      */
     public StateVector solve(Function function, StateVector initialCondition, double t0, double tf, double stepSize) {
 
@@ -42,7 +31,7 @@ public class EulerSolver implements Solver{
             StateVector derivative = function.applyFunction(currentState,t);
 
             //h * f(t,y)
-            StateVector hfty = derivative.multiply(h);
+            StateVector hfty = derivative.multiply(stepSize);
 
             //yn + hf(t,y)
             StateVector y1 = currentState.add(hfty);
@@ -51,11 +40,11 @@ public class EulerSolver implements Solver{
             currentState = y1;
 
             //Update solver time
-            this.t +=h;
+            this.t +=stepSize;
         }
 
         return currentState;
-        
+
     }
 
 }

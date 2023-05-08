5da9cf3383342db1f2c220f5fdb24b8a0a7bbcce
João Victor <j.coutinhoviragine@student.maastrichtuniversity.nl>
2023-05-08 18:25:14 +0200

    Fix Function definition

--- src/physics/functions/Function.java
+++ src/physics/functions/Function.java
@@ -4,6 +4,6 @@ import physics.vectors.StateVector;
 import physics.vectors.Vector;
 
 public interface Function {
-    public StateVector applyFunction(StateVector stateVector, StateVector currentVector, double t);
+    public StateVector applyFunction(StateVector y, double t);
     // Maybe change it to Abstract  to it can be Inherited
 }

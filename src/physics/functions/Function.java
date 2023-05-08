0e578f91525b60d585b1a553cf3e488e592f9840
João Victor <j.coutinhoviragine@student.maastrichtuniversity.nl>
2023-05-08 15:27:40 +0200

    Update Function.java

--- src/physics/functions/Function.java
+++ src/physics/functions/Function.java
@@ -4,6 +4,6 @@ import physics.vectors.StateVector;
 import physics.vectors.Vector;
 
 public interface Function {
-    public StateVector applyFunction(StateVector stateVector);
+    public StateVector applyFunction(StateVector stateVector, StateVector currentVector, double t);
     // Maybe change it to Abstract  to it can be Inherited
 }

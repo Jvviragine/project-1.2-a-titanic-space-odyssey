d8fd510945561d988dbb137f20639b4e65373a5b
tamar <115258119+tamartrbl@users.noreply.github.com>
2023-05-08 12:49:53 +0200

    constructor

--- src/physics/solvers/RK2Solver.java
+++ src/physics/solvers/RK2Solver.java
@@ -3,9 +3,19 @@ import physics.vectors.Vector;
 import physics.vectors.StateVector;
 
 public class RK2Solver implements Solver {
-    StateVector v;
+    StateVector y0;
+    double t0;
+    double t1;
+    double h;
+
+    //fty -> f (replacement later)
+    public RK2Solver(StateVector y0, StateVector fty, double h, double t0){
+        this.y0 = y0;
+        this.t0 = t0;
+        this.h = h;
+    }
 
     public StateVector solve() {
-        return v;
+        return y0;
     }
 }

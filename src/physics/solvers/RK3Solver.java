6ca5f3837794b76ca5e90ba8b12948d4efadbfd9
ulasawczuk <sawczuk.ula@gmail.com>
2023-05-09 10:39:20 +0200

    Update RK3Solver.java

--- src/physics/solvers/RK3Solver.java
+++ src/physics/solvers/RK3Solver.java
@@ -2,7 +2,6 @@ package physics.solvers;
 
 import physics.functions.Function;
 import physics.vectors.StateVector;
-import physics.vectors.Vector;
 
 public class RK3Solver implements Solver{
 
@@ -25,15 +24,17 @@ public class RK3Solver implements Solver{
             //k1 = h*f(t,y)
             StateVector k1 = fty.multiply(stepSize);
 
-            //k2 = h*f(t + 1/3*h,y + 1/3*k1)
-            StateVector k2 = function.applyFunction(fty.add(k1.multiply(1/3)), t + 1/3*stepSize);
+            //k2 = h*f(t + 1/3*h,y + 1/3*k1), getting the derivative than calculating k2
+            StateVector fty2 = function.applyFunction(currentState.add(k1.multiply(1/3)), t + 1/3*stepSize);
+            StateVector k2 = fty2.multiply(stepSize);
 
             //k3 = h*f(t + 2/3*h, y + 2/3*k2)
-            StateVector k3 = function.applyFunction(fty.add(k2.multiply(2/3)), t + 2/3*stepSize);
+            StateVector fty3 = function.applyFunction(currentState.add(k2.multiply(2/3)), t + 2/3*stepSize);
+            StateVector k3 = fty3.multiply(stepSize);
 
-            //yn+1 = y + 1/4*(k1 + 3*k3)
+            //yn+1 = y + 1/4*(k1 + 3*k3), calculating new y than updatin the current state
             StateVector add = k3.multiply(3).add(k1).multiply(1/4);
-            StateVector yNew = fty.add(add);
+            StateVector yNew = currentState.add(add);
             currentState = yNew;
 
         }

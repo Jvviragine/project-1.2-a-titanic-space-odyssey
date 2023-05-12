b170231ace1349f0948a2ab48bea921540546bee
João Victor <j.coutinhoviragine@student.maastrichtuniversity.nl>
2023-05-12 13:15:11 +0200

    RK3 perfectly working and Added Error Comparisson

--- src/physics/solvers/ErrorAnalysisTestFunction.java
+++ src/physics/solvers/ErrorAnalysisTestFunction.java
@@ -22,7 +22,7 @@ public class ErrorAnalysisTestFunction {
 
         // Choosing the Input Values
         double tf = 1;
-        double h = 0.025;
+        double h = 0.05;
 
         // Exact Result of the Differential Equation
         StateVector exactStateVector = ft.applyFunction(initialValue, tf);
@@ -49,7 +49,9 @@ public class ErrorAnalysisTestFunction {
         Solver rungeKutta3Solver = new RK3Solver();
         StateVector rk3StateVector = rungeKutta3Solver.solve(dydt, initialValue, t0, tf, h);
         double rungeKutta3Approximation = rk3StateVector.getVector(0).get(0);
-        System.out.println("RK3 = " + "\n" + rungeKutta3Approximation + "\n");
+        System.out.println("RK3 = " + "\n" + rungeKutta3Approximation);
+        double errorRK3 = Math.abs(exactValue - rungeKutta3Approximation);
+        System.out.println("Error RK3 = " + "\n" + errorRK3 + "\n");
 
         // Approximation using Runge Kutta 4 - Second Order - O(h4)
         Solver rungeKutta4Solver = new RK4Solver();
@@ -58,6 +60,11 @@ public class ErrorAnalysisTestFunction {
         System.out.println("RK4 = " + "\n" + rungeKutta4Approximation);
         double errorRK4 = Math.abs(exactValue - rungeKutta4Approximation);
         System.out.println("Error RK4 = " + "\n" + errorRK4 + "\n");
+
+        // Comparing how bigger the Errors Get
+        System.out.println("Error for Euler is " + (errorEuler/errorRK2) + " times the error for RK2\n");
+        System.out.println("Error for Euler is " + (errorEuler/errorRK3) + " times the error of RK3\n");
+        System.out.println("Error for Euler is " + (errorEuler/errorRK4) + " times the error fof RK4\n");
 
 
     }
--- src/physics/solvers/RK3Solver.java
+++ src/physics/solvers/RK3Solver.java
@@ -29,15 +29,15 @@ public class RK3Solver implements Solver{
             StateVector k1 = fty.multiply(stepSize);
 
             //k2 = h*f(t + 1/3*h,y + 1/3*k1), getting the derivative than calculating k2
-            StateVector fty2 = function.applyFunction(currentState.add(k1.multiply(1/3)), t + 1/3*stepSize);
+            StateVector fty2 = function.applyFunction(currentState.add(k1.multiply(0.333333333333334)), t + 0.333333333333333*stepSize);
             StateVector k2 = fty2.multiply(stepSize);
 
             //k3 = h*f(t + 2/3*h, y + 2/3*k2), getting the derivative than calculating k3
-            StateVector fty3 = function.applyFunction(currentState.add(k2.multiply(2/3)), t + 2/3*stepSize);
+            StateVector fty3 = function.applyFunction(currentState.add(k2.multiply(0.666666666666667)), t + 0.666666666666667*stepSize);
             StateVector k3 = fty3.multiply(stepSize);
 
             //yn+1 = y + 1/4*(k1 + 3*k3), calculating new y and updating the current state
-            StateVector add = k3.multiply(3).add(k1).multiply(1/4);
+            StateVector add = k3.multiply(3.0).add(k1).multiply(0.25);
             currentState = currentState.add(add);
 
         }

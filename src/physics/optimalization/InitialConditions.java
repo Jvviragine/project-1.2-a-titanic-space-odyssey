87808e3440aedc801e951741d736e57c3b9937eb
ulasawczuk <sawczuk.ula@gmail.com>
2023-05-14 13:54:42 +0200

    Update InitialConditions.java

--- src/physics/optimalization/InitialConditions.java
+++ src/physics/optimalization/InitialConditions.java
@@ -1,6 +1,9 @@
 package physics.optimalization;
 
+import physics.vectors.StateVector;
 import physics.vectors.Vector;
+
+import physics.functions.Function;
 
 public class InitialConditions {
 
@@ -24,4 +27,43 @@ public class InitialConditions {
         }
         return point;
     }
+
+//    public void findVelocity(Vector current, Vector probe, Vector titan){
+//        double dist = 0;
+//        int iter = 1000;
+//        while(!closeEnough(probe, titan, dist) && iter>0){
+//            iter--;
+//            gradietDecsent(current, 0.01, );
+//        }
+//    }
+//
+//    public void gradietDecsent(Vector current, double learningRate, Function function){
+//
+//        //calculate the derivative (acceleration)
+//        Vector[] vec = new Vector[1];
+//        vec[0] = current;
+//        StateVector sVec = new StateVector(vec);
+//        StateVector sAcc = function.applyFunction(sVec, 0);
+//        Vector acc = sAcc.getVector(0);
+//
+//        //calculating the error which is the expected output - current output,
+//        // in our case the derivative of the velocity (acceleration) - current velocity
+//        Vector error = error(current, acc);
+//
+//        //y_new = currentVel - learningRate*error
+//        Vector newVel = current.subtract(error.multiply(learningRate));
+//
+//        //checking whether the new velocity gets us close enough to Titan by simulating the trip
+//        //TODO simulation of the trip and checking closeEnough
+//
+//
+//    }
+//
+//    public Vector error(Vector initial, Vector expected){
+//        return expected.subtract(initial);
+//    }
+//
+//    public boolean closeEnough(Vector probe, Vector titan, double dist){
+//        return probe.distance(titan)<dist;
+//    }
 }

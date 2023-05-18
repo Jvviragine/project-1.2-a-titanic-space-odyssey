5b0d2bc4e314d519a1e0878743d80847701e3ab2
AinstvonHowling <g.latimer@student.maastrichtuniversity.nl>
2023-05-18 12:21:21 +0200

    Create FuelUsage.java
    Two different methods of calculating fuel consumption that both require tests. Unsure if it would be effective for the turnaround over Titan, or what inputs would be necessary for that.

---
+++ src/physics/optimalization/FuelUsage.java
@@ -0,0 +1,27 @@
+package physics.optimalization;
+
+public class FuelUsage {
+    private final int ISP = 280; //Specific Impulse taken from a rocket with similar mass (Atlas V)
+    private final int MASS = 50000; //Mass of our rocket in kilograms
+
+    /**
+     * Calculates the fuel consumption in kilograms for any constant maneuver. No code optimization,
+     * since by design only using the rocket thrusters for takeoff, turnaround and landing is optimized.
+     * https://pressbooks.online.ucf.edu/osuniversityphysics/chapter/9-7-rocket-propulsion/
+     * @param startVelocity velocity in m/s when activating thrusters
+     * @param endVelocity velocity in m/s when deactivating thrusters
+     * @param gravity gravitational force of the planet in m/s^2
+     * @param timeframe time in seconds it takes from start to end of the maneuver
+     * @return point on the surface closest to the target
+     */
+    public double CalculateFuel(double startVelocity, double endVelocity, double gravity, double timeframe){
+        double momentum = MASS * Math.abs(endVelocity - startVelocity);
+        double thrust = (momentum / timeframe) - (gravity * MASS);
+        return (thrust / (gravity * ISP)) * timeframe;
+    }
+    public double Alternative(double startVelocity, double endVelocity, double gravity){
+        double e = 2.718;
+        double wetMass = MASS * Math.pow (e, Math.abs(startVelocity - endVelocity) / (ISP * gravity));
+        return wetMass - MASS;
+    }
+}

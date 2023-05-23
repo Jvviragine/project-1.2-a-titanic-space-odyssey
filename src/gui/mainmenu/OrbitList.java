8103e9e34df2665d6bf66c4a91be5c683900c163
tamar <115258119+tamartrbl@users.noreply.github.com>
2023-05-23 12:21:36 +0200

    Added simulation call

--- src/gui/mainmenu/OrbitList.java
+++ src/gui/mainmenu/OrbitList.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 public class OrbitList {
     private static SolarSystemPhysicsSimulation simulation = new SolarSystemPhysicsSimulation(PlanetaryData.getCelestialBodiesStateVector(),PlanetaryData.getCelestialBodiesMasses(),PlanetaryData.getCelestialBodyNames());
-    private static List<List<StateVector>> planetPaths = simulation.getPath();
+    private static List<List<StateVector>> planetPaths = simulation.simulateCelestialBodiesOrbit(31536000,360);
 
     public static void main(String[] args) {
         System.out.println(planetPaths.get(0).get(0).getVector(0).get(2));

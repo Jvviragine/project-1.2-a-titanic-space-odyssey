28a14fd3bafe92884f4d2f034d6f58c706e61372
LucWehrenss <lj.wehrens@student.maastrichtuniversity.nl>
2023-05-24 15:47:56 +0200

    added all planets and moons for testing

--- src/gui/mainmenu/SimulationScreen.java
+++ src/gui/mainmenu/SimulationScreen.java
@@ -19,17 +19,20 @@ public class SimulationScreen extends JPanel {
 
     private int currentIndex = 0;
     private Timer timer;
+
     private OrbitList orbitList = new OrbitList();
     private int[][] sunPath = orbitList.getPath(0);
     private int[][] mercuryPath = orbitList.getPath(1);
     private int[][] venusPath = orbitList.getPath(2);
     private int[][] earthPath = orbitList.getPath(3);
-    private int[][] marsPath = orbitList.getPath(4);
-    private int[][] jupiterPath = orbitList.getPath(5);
-    private int[][] saturnPath = orbitList.getPath(6);
-    private int[][] uranusPath = orbitList.getPath(7);
-    private int[][] neptunePath = orbitList.getPath(8);
-    private int[][][] allPaths = {sunPath, mercuryPath, venusPath, marsPath, jupiterPath, saturnPath, uranusPath, neptunePath};
+    private int[][] moonPath = orbitList.getPath(4);
+    private int[][] marsPath = orbitList.getPath(5);
+    private int[][] jupiterPath = orbitList.getPath(6);
+    private int[][] saturnPath = orbitList.getPath(7);
+    private int[][] titanPath = orbitList.getPath(8);
+    private int[][] uranusPath = orbitList.getPath(9);
+    private int[][] neptunePath = orbitList.getPath(10);
+    private int[][][] allPaths = {sunPath, mercuryPath, venusPath, earthPath, moonPath, marsPath, jupiterPath, saturnPath, titanPath, uranusPath, neptunePath};
 
     public SimulationScreen() {
         frame = new JFrame("Simulation Screen");
@@ -61,20 +64,47 @@ public class SimulationScreen extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
+        //Sun
         g2d.setColor(Color.YELLOW);
         g2d.fillOval(XCENTER + sunPath[currentIndex][0] - 20, YCENTER + sunPath[currentIndex][1] - 20, 40, 40);
+
+        //Mercury
+        g2d.setColor(Color.GRAY);
+        g2d.fillOval(XCENTER + mercuryPath[currentIndex][0] - 10,  YCENTER + mercuryPath[currentIndex][1] - 10, 20, 20);
+
+        //Venus
         g2d.setColor(Color.ORANGE);
         g2d.fillOval(XCENTER + venusPath[currentIndex][0] - 10,  YCENTER + venusPath[currentIndex][1] - 10, 20, 20);
+
+        //Earth
         g2d.setColor(Color.BLUE);
         g2d.fillOval(XCENTER + earthPath[currentIndex][0] - 10,  YCENTER + earthPath[currentIndex][1] - 10, 20, 20);
+
+        //Moon
+        g2d.setColor(Color.WHITE);
+        g2d.fillOval(XCENTER + moonPath[currentIndex][0] - 10,  YCENTER + moonPath[currentIndex][1] - 10, 10, 10);
+
+        //Mars
         g2d.setColor(Color.RED);
         g2d.fillOval(XCENTER + marsPath[currentIndex][0] - 10,  YCENTER + marsPath[currentIndex][1] - 10, 20, 20);
+
+        //Jupiter
         g2d.setColor(Color.ORANGE);
         g2d.fillOval(XCENTER + jupiterPath[currentIndex][0] - 10,  YCENTER + jupiterPath[currentIndex][1] - 10, 20, 20);
+
+        //Saturn
         g2d.setColor(Color.YELLOW);
         g2d.fillOval(XCENTER + saturnPath[currentIndex][0] - 10,  YCENTER + saturnPath[currentIndex][1] - 10, 20, 20);
+
+        //Titan
+        g2d.setColor(Color.WHITE);
+        g2d.fillOval(XCENTER + titanPath[currentIndex][0] - 10,  YCENTER + titanPath[currentIndex][1] - 10, 10, 10);
+
+        //Uranus
         g2d.setColor(Color.LIGHT_GRAY);
         g2d.fillOval(XCENTER + uranusPath[currentIndex][0] - 10,  YCENTER + uranusPath[currentIndex][1] - 10, 20, 20);
+
+        //Neptune
         g2d.setColor(Color.BLUE);
         g2d.fillOval(XCENTER + neptunePath[currentIndex][0] - 10,  YCENTER + neptunePath[currentIndex][1] - 10, 20, 20);
     }
@@ -87,8 +117,7 @@ public class SimulationScreen extends JPanel {
             if (currentIndex >= mercuryPath.length) {
                 currentIndex = 0;
             }
-            //System.out.println(sunPath[currentIndex][0] + " " + sunPath[currentIndex][1]);
-            //System.out.println(mercuryPath[currentIndex][0] + " " + mercuryPath[currentIndex][1]);
+
             repaint();
         }
 

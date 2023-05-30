d6887d2e036c42455acde7760c8da8afe951838f
ulasawczuk <sawczuk.ula@gmail.com>
2023-05-30 17:07:39 +0200

    trying the trip

--- src/physics/optimalization/FuelUsage.java
+++ src/physics/optimalization/FuelUsage.java
@@ -2,7 +2,7 @@ package physics.optimalization;
 
 public class FuelUsage {
     //private static final int ISP = 280; //Specific Impulse taken from a rocket with similar mass (Atlas V)
-    private final int MASS = 50000; //Mass of our rocket in kilograms
+    private static final int MASS = 50000; //Mass of our rocket in kilograms
 
 //    public static double calculateFuel(double startVelocity, double endVelocity, double gravity, double timeframe){
 //        double momentum = MASS * Math.abs(endVelocity - startVelocity);
@@ -32,7 +32,7 @@ public class FuelUsage {
     public double fuel(double startVelocity, double endVelocity, double timeframe){
         return Math.abs(impulse(startVelocity, endVelocity, timeframe)) * 0.001; //1m/s is 0.001km/s, fuel consumption is proportional to impulse
     }
-    public double fuelTakeoffLanding(double startVelocity, double endVelocity, double timeframe){
+    public static double fuelTakeoffLanding(double startVelocity, double endVelocity, double timeframe){
         return Math.abs(impulseTakeoffLanding(startVelocity, endVelocity, timeframe)) * 0.001;
     }
 
@@ -46,7 +46,7 @@ public class FuelUsage {
     public double impulse(double startVelocity, double endVelocity, double timeframe){
         return force(startVelocity, endVelocity, timeframe) * timeframe; //For a constant force over time, one can choose to solve the integral graphically (form of a rectangle)
     }
-    public double impulseTakeoffLanding(double startVelocity, double endVelocity, double timeframe){
+    public static double impulseTakeoffLanding(double startVelocity, double endVelocity, double timeframe){
         return forceTakeoffLanding(startVelocity, endVelocity, timeframe) * timeframe;
     }
 
@@ -60,7 +60,7 @@ public class FuelUsage {
     public double force(double startVelocity, double endVelocity, double timeframe){
         return MASS * acceleration(startVelocity, endVelocity, timeframe); //F=m*a
     }
-    public double forceTakeoffLanding(double startVelocity, double endVelocity, double timeframe){
+    public static double forceTakeoffLanding(double startVelocity, double endVelocity, double timeframe){
         return MASS * acceleration(startVelocity, endVelocity, timeframe) + 9.81; //F=m*a+g
     }
 
@@ -71,7 +71,7 @@ public class FuelUsage {
      * @param timeframe time in seconds it takes from start to end of the maneuver
      * @return constant acceleration
      */
-    public double acceleration(double startVelocity, double endVelocity, double timeframe){
+    public static double acceleration(double startVelocity, double endVelocity, double timeframe){
         return Math.abs(startVelocity - endVelocity) / timeframe; //Change in speed over a timeframe; assumes a constant acceleration
     }
 
--- src/physics/simulation/TripSimulation.java
+++ src/physics/simulation/TripSimulation.java
@@ -3,10 +3,24 @@ package physics.simulation;
 import gui.screens.StartScreen;
 import physics.optimalization.Corrections;
 import physics.vectors.StateVector;
+import physics.vectors.Vector;
 import solar_system_data.InitialConditions;
 import solar_system_data.PlanetaryData;
+import physics.optimalization.FuelUsage;
+
+import javax.swing.plaf.nimbus.State;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TripSimulation {
+
+//    SolarSystemPhysicsSimulation system;
+//
+//    public TripSimulation(SolarSystemPhysicsSimulation system){
+//        this.system = system;
+//    }
+    double usedFuel = 0;
     public void simulate(){
         //Create a new simulation
         SolarSystemPhysicsSimulation simulation = new SolarSystemPhysicsSimulation(PlanetaryData.getCelestialBodiesStateVector(),PlanetaryData.getCelestialBodiesMasses(),PlanetaryData.getCelestialBodyNames());
@@ -42,18 +56,92 @@ public class TripSimulation {
         }
     }
 
-     public void simulateTrip(SolarSystemPhysicsSimulation system){
-        //first thrust, change of velocoty from 0 to initial
+     public List<List<StateVector>> simulateTrip(){
 
-        //start of the sinulation until 365 days with the starting state vectors the positions after probe exits earth
+         List<List<StateVector>> orbits;
+         List<List<StateVector>> finalOrbits = new ArrayList<>();
+         int tf = StartScreen.simulationEndTime;
+         double h = StartScreen.h;
 
-        //when entered titans orbit change velocity to 0(secind thrust)
+         SolarSystemPhysicsSimulation simulationForSec = new SolarSystemPhysicsSimulation(PlanetaryData.getCelestialBodiesStateVector(),PlanetaryData.getCelestialBodiesMasses(),PlanetaryData.getCelestialBodyNames());
+         simulationForSec.simulateOrbitsWithProbe(InitialConditions.getProbeInitialState(), 31536000,360);
 
-        //find the direction vector to earth
+         //calculating the time needed to exit earth to know how much fuel we use
+         double sec = 0;
+         for(int i = 0; i<simulationForSec.getPath().get(0).size();i++){
+             double dist = simulationForSec.getPath().get(4).get(i).getVector(0).distance(simulationForSec.getPath().get(11).get(i).getVector(0));
+             if(dist>6563000){
+                 sec = i*360;
+                 break;
+             }
+         }
 
-        //last thrust, change velocoty from 0 to direction of the earth
+         //calculating used fuel and setting new velocity
+         Vector velF  = new Vector(new double[]{46.18781669742928, -44.638735761874045, -2.9953741584880706});
+         double sub = FuelUsage.fuelTakeoffLanding(InitialConditions.getInitialProbeVelocity().getMagnitude(), velF.getMagnitude(), sec);
+         usedFuel = usedFuel + sub;
 
-        //sinulate for kne year
+         //InitialConditions.setProbeInitialVelocity(velF);
 
+         //start of the simulation
+         SolarSystemPhysicsSimulation simulation = new SolarSystemPhysicsSimulation(PlanetaryData.getCelestialBodiesStateVector(),PlanetaryData.getCelestialBodiesMasses(),PlanetaryData.getCelestialBodyNames());
+         orbits = simulation.simulateOrbitsWithProbe(InitialConditions.getProbeInitialState(), 31536000,360);
+         System.out.println(orbits.get(0).size());
+
+
+
+
+         for(int i = 0;i<orbits.get(0).size();i++){
+            //distance from the probe to titan
+            double dist = orbits.get(11).get(i).getVector(0).distance(orbits.get(8).get(i).getVector(0));
+            //checinkg if got to titan if so than setting up the simulation to get back, we never do??;(((((
+            if(dist<300000){
+                sec = i*360;
+                System.out.println(sec);
+                //making new state vectors for new simulation
+                StateVector[] newStateVectors = new StateVector[simulation.getStateVectors().length];
+                for(int j = 0; j<newStateVectors.length;i++){
+                    newStateVectors[i] = simulation.getPath().get(j).get(i);
+                }
+
+                //stopping the probe, calculating fuel
+                velF  = new Vector(new double[]{0,0,0});
+                sub = FuelUsage.fuelTakeoffLanding(simulation.getPath().get(11).get(i).getVector(1).getMagnitude(), velF.getMagnitude(), 0);
+                usedFuel = usedFuel + sub;
+
+                //getting new velocity for the probe
+                Corrections correct = new Corrections();
+
+                //Adjust new coordinates
+                StateVector newProbeState = correct.adjust(simulation.getPath().get(11).get(i),simulation.getPath().get(8).get(i),sec);
+
+                //setting up new simulation and running it with new State for probe (on titan)
+                double newSimulationTime = 31536000-sec;
+                SolarSystemPhysicsSimulation adjustedSimulation = new SolarSystemPhysicsSimulation(newStateVectors,PlanetaryData.getCelestialBodiesMasses(),PlanetaryData.getCelestialBodyNames());
+                List<List<StateVector>> adjustedOrbits = new ArrayList<>();
+                adjustedOrbits = adjustedSimulation.simulateOrbitsWithProbe(newProbeState,newSimulationTime,360);
+                System.out.println(adjustedOrbits.size());
+
+                //creating the final orbits list
+                for(int j=0; j<orbits.size();j++){
+                    finalOrbits.add(new ArrayList<>());
+                    int diff = orbits.size()-adjustedOrbits.size();
+                    for(int k=0; k<orbits.get(j).size();k++){
+                        StateVector sv = orbits.get(j).get(k);
+                        if(k>=diff){
+                            sv = adjustedOrbits.get(j).get(k - diff);
+                        }
+                        finalOrbits.get(j).add(sv);
+                    }
+                }
+            }
+         }
+         return finalOrbits;
      }
+
+    public static void main(String[] args) {
+        TripSimulation sim = new TripSimulation();
+        List<List<StateVector>> orb = sim.simulateTrip();
+        System.out.println(orb.size());
+    }
 }

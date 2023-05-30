f7efdbc834b2246aa54f0f4bdf3e291fa8282401
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2023-05-30 10:18:08 +0200

    Update TripSimulation.java

--- src/physics/simulation/TripSimulation.java
+++ src/physics/simulation/TripSimulation.java
@@ -3,6 +3,17 @@ package physics.simulation;
 public class TripSimulation {
 
      public void simulateTrip(SolarSystemPhysicsSimulation system){
+        //first thrust, change of velocoty from 0 to initial
+
+        //start of the sinulation until 365 days with the starting state vectors the positions after probe exits earth
+
+        //when entered titans orbit change velocity to 0(secind thrust)
+
+        //find the direction vector to earth
+
+        //last thrust, change velocoty from 0 to direction of the earth
+
+        //sinulate for kne year
 
      }
 }

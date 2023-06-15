892d41cc658bf4607341bfa7271874c90c068744
AllanCelebioglu <celebiogluallan@gmail.com>
2023-06-15 11:44:25 +0200

    InitialConditionsTest updated

--- src/test/solar_system_data/InitialConditionsTest.java
+++ src/test/solar_system_data/InitialConditionsTest.java
@@ -1,13 +1,27 @@
 package solar_system_data;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import physics.vectors.StateVector;
 import physics.vectors.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+/*
+This class saves the initial condition state vectors in static attributes.
+To test the methods that apply changes on those static fields, this test class
+will recover each static values and will save them to recover any changes made
+
+This is how it works :
+
+1- init() : saves the static attributes of InitialConditions in local variables
+
+2- setUp() : set the static attributes of the InitialConditions class to their
+    initial values from the local variables.
+    Run a test and repeat this second test until all tests are covered
+
+3- cleanUp() : reset the InitialConditions values one last time so that no static
+    values are changed from the tested class
+ */
 
 class InitialConditionsTest {
 
@@ -21,6 +35,13 @@ class InitialConditionsTest {
         initialProbePosition = InitialConditions.getInitialProbePosition();
         initialProbeVelocity = InitialConditions.getInitialProbeVelocity();
         mass = InitialConditions.getProbeMass();
+    }
+
+    @AfterAll
+    //Restores the correct values to the static fields after all the tests
+    static void cleanUp() {
+        InitialConditions.setProbeInitialPosition(initialProbePosition);
+        InitialConditions.setProbeInitialVelocity(initialProbeVelocity);
     }
 
     @BeforeEach
@@ -62,6 +83,7 @@ class InitialConditionsTest {
     }
 
     @Test
+    //cover setProbeInitialVelocity
     void testSetProbeInitialVelocity() {
         double[] expected = new double[]{0, -1, 2, 3.5};
         InitialConditions.setProbeInitialVelocity(new Vector(expected));
@@ -72,6 +94,7 @@ class InitialConditionsTest {
     }
 
     @Test
+    //cover getProbeInitialState
     void testGetProbeInitialState() {
         Vector[] expected = new Vector[]{initialProbePosition, initialProbeVelocity};
         StateVector output = InitialConditions.getProbeInitialState();

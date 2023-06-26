7e6628325b126f2ab201b5e199b98cc7ee609e84
LucWehrenss <lj.wehrens@student.maastrichtuniversity.nl>
2023-06-26 12:05:49 +0200

    runTimeExperiment class added

--- src/physics/solvers/RunTimeExperiment.java
+++ src/physics/solvers/RunTimeExperiment.java
@@ -1,9 +1,27 @@
 package physics.solvers;
 
+import physics.simulation.SolarSystemPhysicsSimulation;
 import solar_system_data.PlanetaryData;
 
 public class RunTimeExperiment {
 
-    // Creating an Instance of the Celestial Bodies
+    private Solver eulerSolver = new EulerSolver();
+    private Solver RK2 = new RK2Solver();
+    private Solver RK3 = new RK3Solver();
+    private Solver RK4 = new RK4Solver();
 
+    public RunTimeExperiment() {
+
+        SolarSystemPhysicsSimulation system = new SolarSystemPhysicsSimulation(PlanetaryData.getCelestialBodiesStateVector(),PlanetaryData.getCelestialBodiesMasses(),PlanetaryData.getCelestialBodyNames(), RK4);
+        system.simulateCelestialBodiesOrbit(36000000, 60);
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        RunTimeExperiment runTimeExperiment = new RunTimeExperiment();
+
+        long stopTime = System.nanoTime();
+        System.out.println((stopTime - startTime)/1000);
+    }
 }
